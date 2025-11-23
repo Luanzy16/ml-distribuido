88221c2568e006616e1ed84d6cd8cae76447a478
Luis <luanzy1610@gmail.com>
2025-11-23 17:01:57 -0500

    push

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # .env debe existir
-if [ ! -f /app/.env ]; then
+if [ ! -f .env ]; then
   echo ".env not found. Copy .env.template to .env and set variables."
   exec sleep infinity
 fi
